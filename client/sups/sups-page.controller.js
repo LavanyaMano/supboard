dc1ec73b27ad79dbcc6f9c02d321b896e5d075dc
lava m <lavanymkanna@gmail.com>
2016-10-12 15:24:35 -0400

    supboard items created

---
+++ client/sups/sups-page.controller.js
@@ -0,0 +1,10 @@
+function SupsPageController(){
+    const ctrl = this;
+
+    ctrl.sup ={
+        text: 'Hey what up?',
+        created_date: Date.now(),
+    };
+}
+
+export default SupsPageController;
